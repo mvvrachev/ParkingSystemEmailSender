e88c1976478564c6716044176a617e19af8c4691
mvvrachev <martin.v.vrachev.2017@elsys-bg.org>
2022-04-02 23:02:18 +0300

    Added firebase connection and reservations info to email content

--- EmailSender/src/main/java/email_sender/EmailSender.java
+++ EmailSender/src/main/java/email_sender/EmailSender.java
@@ -1,13 +1,71 @@
 package email_sender;
 
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class EmailSender {
     public static void main(String[] args) {
-// Recipient's email ID needs to be mentioned.
+
+        FileInputStream serviceAccount =
+                null;
+        try {
+            serviceAccount = new FileInputStream("EmailSender\\parkingsystem-cf164-firebase-adminsdk-5jjuk-2be72bfcce.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        FirebaseOptions options = null;
+        try {
+            options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FirebaseApp.initializeApp(options);
+
+        Firestore db = FirestoreClient.getFirestore();
+
+        // asynchronously retrieve all documents
+        ApiFuture<QuerySnapshot> future = db.collection("reservations").whereEqualTo("date", getTodayDate()).get();
+        // future.get() blocks on response
+        List<QueryDocumentSnapshot> documents = null;
+        try {
+            documents = future.get().getDocuments();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder reservations = new StringBuilder();
+        for (QueryDocumentSnapshot document : documents) {
+            reservations.append(document.getData()).append("\n");
+        }
+
+
+        // Recipient's email ID needs to be mentioned.
         String to = "parkingsystem4@gmail.com";
 
         // Sender's email ID needs to be mentioned
@@ -50,14 +108,22 @@ public class EmailSender {
             message.setSubject("This is the Subject Line!");
 
             // Now set the actual message
-            message.setText("This is actual message");
+            message.setText(String.valueOf(reservations));
 
             System.out.println("sending...");
             // Send message
             Transport.send(message);
-            System.out.println("Sent message successfully....");
+            System.out.println("Sent email successfully!");
         } catch (MessagingException mex) {
             mex.printStackTrace();
         }
     }
+
+    private static String getTodayDate() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        Calendar calendar = Calendar.getInstance();
+
+        return formatter.format(calendar.getTime());
+    }
 }
+
