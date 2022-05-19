ff5ff4d6c81315f4828675c742060cc761dec53f
mvvrachev <martin.v.vrachev.2017@elsys-bg.org>
2022-05-19 16:28:47 +0300

    Fix email content formatting

--- EmailSender/src/main/java/email_sender/EmailSender.java
+++ EmailSender/src/main/java/email_sender/EmailSender.java
@@ -2,7 +2,6 @@ package email_sender;
 
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
@@ -13,13 +12,9 @@ import com.google.firebase.cloud.FirestoreClient;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
@@ -28,6 +23,7 @@ import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 public class EmailSender {
+
     public static void main(String[] args) {
 
         FileInputStream serviceAccount = null;
@@ -50,70 +46,54 @@ public class EmailSender {
 
         Firestore db = FirestoreClient.getFirestore();
 
-        // asynchronously retrieve all documents
         ApiFuture<QuerySnapshot> future = db.collection("reservations").whereEqualTo("date", getTodayDate()).get();
-        // future.get() blocks on response
+
         List<QueryDocumentSnapshot> documents = null;
         try {
             documents = future.get().getDocuments();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
 
-        StringBuilder reservations = new StringBuilder();
+        StringBuilder reservations = new StringBuilder("Reservations for today: \n\n");
         for (QueryDocumentSnapshot document : documents) {
-            reservations.append(document.getData()).append("\n");
+            reservations.append("Space: ").append(document.getData().get("space")).append("\n")
+                    .append("Floor: ").append(document.getData().get("floor")).append("\n")
+                    .append("Car Number: ").append(document.getData().get("carNumber")).append("\n")
+                    .append("\n");
         }
 
-        // Recipient's email ID needs to be mentioned.
         String to = "parkingsystem4@gmail.com";
-
-        // Sender's email ID needs to be mentioned
         String from = "parkingsystem4@gmail.com";
 
-        // Assuming you are sending email from through gmails smtp
         String host = "smtp.gmail.com";
 
-        // Get system properties
         Properties properties = System.getProperties();
 
-        // Setup mail server
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", "465");
         properties.put("mail.smtp.ssl.enable", "true");
         properties.put("mail.smtp.auth", "true");
 
-        // Get the Session object.// and pass username and password
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
 
             protected PasswordAuthentication getPasswordAuthentication() {
-
                 return new PasswordAuthentication("parkingsystem4@gmail.com", "parkingsystem123");
-
             }
-
         });
 
         try {
-            // Create a default MimeMessage object.
             MimeMessage message = new MimeMessage(session);
 
-            // Set From: header field of the header.
             message.setFrom(new InternetAddress(from));
 
-            // Set To: header field of the header.
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
-            // Set Subject: header field
             message.setSubject("This is the Subject Line!");
 
-            // Now set the actual message
             message.setText(String.valueOf(reservations));
 
             System.out.println("sending...");
-            // Send message
             Transport.send(message);
             System.out.println("Sent email successfully!");
         } catch (MessagingException mex) {
@@ -122,7 +102,6 @@ public class EmailSender {
     }
 
     private static String getTodayDate() {
-
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
         Calendar calendar = Calendar.getInstance();
 
